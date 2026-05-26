c0da980a017bfa465de2066036f470d80ceb64e9
Irvin Pineda <00024420@uca.edu.sv>
2026-05-25 20:16:36 -0600

    FIX BUG

--- src/main/java/org/example/parcial02/repositories/BookRepository.java
+++ src/main/java/org/example/parcial02/repositories/BookRepository.java
@@ -1,18 +1,27 @@
 package org.example.parcial02.repositories;
 
-import jakarta.validation.constraints.NotBlank;
+
+
+
 import org.example.parcial02.domain.entities.Book;
 import org.example.parcial02.domain.enums.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    boolean existsByTitleIgnoreCase(String title);
+
+    boolean existsByIsbn(String isbn);
+
+    @Query("SELECT b FROM Book b WHERE " +
+            "(:genre IS NULL OR b.genre = :genre) AND " +
+            "(:available IS NULL OR b.available = :available)")
     List<Book> findByFilters(@Param("genre") Genre genre, @Param("available") Boolean available);
 
-    boolean existsByIsbn(@NotBlank(message = "ISBN es requerido") String isbn);
-
-    boolean existsByTitleIgnoreCase(@NotBlank(message = "Se necesita el titulo") String title);
 }
